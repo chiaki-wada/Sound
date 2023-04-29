c14a6ba7c9ba0f31ed54f1f30ff2399737cf91ae
chiaki-wada <zpd2da.zipadeeay@gmail.com>
2023-04-30 00:02:48 +0900

    Second Commit

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -10,7 +10,8 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
-    
+    //ドラムを表示する箱をつくる
+    @IBOutlet var drumButton: UIButton!
     
     //ドラムのサウンドファイルを読み込んで、プレイヤーをつくる
     let drumSoundPlayer = try!AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
@@ -20,14 +21,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    //ドラムがタップされたときに呼ばれる
-    @IBAction func tapDrumButton(){
+    //ドラムのタッチが始まった時に呼ばれる
+    @IBAction func touchDownDrumButton(){
+        
+        //ドラムが鳴っている画像に切り替える
+        drumButton.setImage(UIImage(named: "drumPlayingImage"), for: .normal)
         
         //ドラムの音を巻き戻す
         drumSoundPlayer.currentTime = 0
         
         //ドラムの音を再生する
         drumSoundPlayer.play()
+    }
+    
+    //ドラムボタンの上でタッチが終わる時に呼ばれる
+    @IBAction func touchUpDrumButton() {
+        
+        //ドラムが鳴っていない画像に切り替える
+        drumButton.setImage(UIImage(named: "drumImage"), for: .normal)
         
     }
 }
